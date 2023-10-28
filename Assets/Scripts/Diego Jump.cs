8b630fdff0db8ffb36814c4e908aeabba685e9ca
Diego Patiño <dpatino@umich.edu>
2023-10-28 14:48:51 -0400

    Diego Wall Jumping
    made a scene to try and develop wall jump mechanics

---
+++ Assets/Scripts/Diego Jump.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiegoJump : MonoBehaviour
+{
+
+    public LayerMask groundLayer;
+    public float jumpForce = 5f;
+    public float wallJumpForce = 5f;
+    public float wallSlideSpeed = 2f;
+
+    private bool isWallSliding;
+    public Transform wallCheckL;
+    public Transform wallCheckR;
+    public Transform groundCheck;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Wall sliding check
+        if(IsTouchingWall() && !IsGrounded()) {
+            isWallSliding = true;
+            //rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+        }
+        else {
+            isWallSliding = false;
+        }
+
+        // Jump button pressed
+        if(Input.GetButtonDown("Jump")) {
+            // Wall Jump
+            if(isWallSliding) {
+                WallJump();
+            }
+            else {
+                Jump();
+            }
+        }
+    }
+
+    void Jump() {
+        if(IsGrounded()) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void WallJump() {
+        Vector2 wallJumpDir = new Vector2(-transform.localScale.x, 1f);
+        rb.AddForce(wallJumpDir * wallJumpForce, ForceMode2D.Impulse);
+        //rb.velocity = new Vector2(rb.velocity.x, wallJumpForce);
+    }
+
+    bool IsGrounded() {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+    bool IsTouchingWall() {
+        return  Physics2D.OverlapCircle(wallCheckL.position, 0.1f, groundLayer) ||
+                Physics2D.OverlapCircle(wallCheckR.position, 0.1f, groundLayer);
+    }
+}
