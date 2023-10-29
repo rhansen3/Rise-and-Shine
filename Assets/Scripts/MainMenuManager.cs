ad9609d6eb201150aa605697cd9b1eb8513f276e
rhansen3 <rjhansen@umich.edu>
2023-10-29 16:11:53 -0400

    Added TMP and Main Menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,18 @@
+#if Unity_Editor
+using UnityEngine;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
